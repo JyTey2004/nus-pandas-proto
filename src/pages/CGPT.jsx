df7151d2741fc70e9d80bb1023aacf842dde4566
Harsh Sharma <harshvs415@gmail.com>
2024-10-29 04:57:34 +0800

    added cgpt starter

--- src/pages/CGPT.jsx
+++ src/pages/CGPT.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import CGPTBackground from '../components/backgrounds/CGPTBackground'
+import CGPTSection from '../components/sections/CGPTSection'
+
+import ChatBox from '../components/ChatBox/ChatBox'
+
+import RowFeatureCard from '../components/cards/RowFeatureCard'
 
 const CGPT = () => {
     return (
-        <div>
-
-        </div>
+        <CGPTBackground>
+            <CGPTSection>
+                <ChatBox />
+                <div className='feature-container'>
+                    <div className='feature-card-container'>
+                        <RowFeatureCard title="22,000+ Datasets" description="Access a vast collection of diverse, high-quality datasets from various industries." direction='left' />
+                    </div>
+                </div>
+            </CGPTSection>
+        </CGPTBackground>
     )
 }
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,7 +4,6 @@ import NavigationBar from '../components/navigation/NavigationBar.jsx';
 
 import Home from '../pages/Home.jsx';
 import CGPT from '../pages/CGPT.jsx';
-import HGPT from '../pages/HGPT.jsx';
 import InsurerDashboard from '../pages/InsurerDashboard.jsx';
 
 const AppRouter = () => {
@@ -14,7 +13,6 @@ const AppRouter = () => {
                 <Route element={<NavigationBar />}>
                     <Route path="/" element={<Home />} />
                     <Route path="/C-GPT" element={<CGPT />} />
-                    <Route path="/H-GPT" element={<HGPT />} />
                     <Route path="/insurer" element={<InsurerDashboard />} />
                 </Route>
             </Routes>
